5219f0ece0200da3e2e74f632166c99481c14ae4
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-16 01:51:38 +0530

    error-handling

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -18,6 +18,8 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  
+  
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
     <form action={updateInvoiceWithId}>
